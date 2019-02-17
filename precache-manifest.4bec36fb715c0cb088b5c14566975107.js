0b566405848c018456c6bc4f54f5c07f70a9780a
YoungBrick <tidorka@gmail.com>
2019-02-17 16:16:47 +0300

    feat: add built project

---
+++ precache-manifest.4bec36fb715c0cb088b5c14566975107.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "dbd6e1822f4200a1a125",
+    "url": "/static/js/main.dbd6e182.chunk.js"
+  },
+  {
+    "revision": "79e83b158b3618116c15",
+    "url": "/static/js/1.79e83b15.chunk.js"
+  },
+  {
+    "revision": "dbd6e1822f4200a1a125",
+    "url": "/static/css/main.d29be6d4.chunk.css"
+  },
+  {
+    "revision": "2381ce0948256964dd41b6b5c14b6abb",
+    "url": "/index.html"
+  }
+];
